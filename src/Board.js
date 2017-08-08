b24f4241d31807f373f985bf834374bb39c3bd8a
Daniel <danthemanb3@gmail.com>
2017-08-07 20:49:38 -0600

    Added react components for us to base code off of. No real functionality

---
+++ src/Board.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import CardPile from './CardPile.js';
+
+class Board extends Component {
+    render() {
+        return (
+            <div id="Board">
+                <div className="topRow">
+                    <CardPile />
+                    <CardPile />
+                    <CardPile />
+                </div>
+                <div className="middleRow">
+                    <CardPile />
+                    <CardPile />
+                    <CardPile />
+                </div>
+                <div className="bottomRow">
+                    <CardPile />
+                    <CardPile />
+                    <CardPile />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
---
+++ src/CardPile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CardPile extends Component {
+    render () {
+        return (
+        <div className="cardPile">
+            cardPile
+        </div>
+        );
+    }
+}
+
+export default CardPile;
